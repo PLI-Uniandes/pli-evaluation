9895b3bbeacdc852fe7b861e9788cda26e06d6bf
jdfandino10 <jd.fandino10@uniandes.edu.co>
2018-08-14 10:33:28 -0500

    removed tasks related stuff

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { withTracker } from 'meteor/react-meteor-data';
-import { Tasks } from '../api/tasks.js';
-import Task from './Task.js';
 import LoginButton from './LoginButton.js';
 import Graph from './Graph';
 import { Users } from '../api/users';
@@ -19,31 +17,6 @@ class App extends Component {
     };
   }
 
-    handleSubmit(event) {
-    event.preventDefault();
- 
-    // Find the text field via the React ref
-    const text = ReactDOM.findDOMNode(this.refs.textInput).value.trim();
- 
-    Tasks.insert({
-      text,
-      createdAt: new Date(), // current time
-    });
- 
-    // Clear form
-    ReactDOM.findDOMNode(this.refs.textInput).value = '';
-  }
- 
-  renderTasks() {
-    let filteredTasks = this.props.tasks;
-    if (this.state.hideCompleted) {
-      filteredTasks = filteredTasks.filter(task => !task.checked);
-    }
-    return filteredTasks.map((task) => (
-      <Task key={task._id} task={task} />
-    ));
-  }
-
   toggleHideCompleted() {
     this.setState({
       hideCompleted: !this.state.hideCompleted,
@@ -58,31 +31,9 @@ class App extends Component {
     return (
       <div className="container">
         <header>
-          <h1>Todo List ({this.props.incompleteCount})</h1>
           <LoginButton currentUser={this.props.currentUser}/>
-          <div className="col-md-4 pull-right">
-            <label className="hide-completed">
-              <input
-                type="checkbox"
-                readOnly
-                checked={this.state.hideCompleted}
-                onClick={this.toggleHideCompleted.bind(this)}
-              />
-              Hide Completed Tasks
-            </label>
-          </div>
-          <form className="new-task" onSubmit={this.handleSubmit.bind(this)} >
-            <input
-              type="text"
-              ref="textInput"
-              placeholder="Type to add new tasks"
-            />
-          </form>
-        </header> 
-        <ul>
-          {this.renderTasks()}
-        </ul>
-          {this.renderUserGraph()}
+        </header>
+        {this.renderUserGraph()}
       </div>
     );
   }
@@ -91,8 +42,6 @@ class App extends Component {
 export default withTracker(() => {
   const handleUsers = Meteor.subscribe('users.all');
   return {
-    tasks: Tasks.find({}, { sort: { createdAt: -1 } }).fetch(),
-    incompleteCount: Tasks.find({ checked: { $ne: true } }).count(),
     currentUser: Meteor.user(),
     loading: !handleUsers.ready(),
     users: Users.fetch()
--- server/main.js
+++ server/main.js
@@ -1,4 +1,3 @@
-import '../imports/api/tasks.js';
 import '../imports/api/users.js';
 import './accounts.js';
 import './publish.js';
