d6e96d43651c0a688398566891558d7f3fd9d0cc
dalthviz <d.althviz10@uniandes.edu.co>
2018-08-23 23:46:26 -0500

    Code style

--- imports/ui/EvaluationEditor.js
+++ imports/ui/EvaluationEditor.js
@@ -82,7 +82,7 @@ class EvaluationEditor extends Component {
       });
     } else {
       Meteor.call("evaluationForms.newEvaluationForm", this.state.editor.text, (err, result) => {
-        if (err) this.props.alert.error(err.message);
+        if (err) { this.props.alert.error(err.message) }
         else {
           this.props.alert.success("Formato guardado exitosamente");
           this.setInList(true);
